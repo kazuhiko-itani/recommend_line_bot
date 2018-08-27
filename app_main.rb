30eb90c78797f152982a26c23ae73660178fc357
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-27 23:19:14 +0900

    add basic auth

--- app_main.rb
+++ app_main.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 require 'line/bot'
 require 'uri'
 
+use Rack::Auth::Basic do |username, password|
+  username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+end
+
 get '/' do
 	'hello!'
 end
