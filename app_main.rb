af1d9fd4329d109c1a3339a13b12e6ab64c7dfdb
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-27 16:01:07 +0900

    modify

--- app_main.rb
+++ app_main.rb
@@ -28,7 +28,7 @@ post '/callback' do
           when Line::Bot::Event::MessageType::Text
             message = {
               type: 'text',
-              text: event.source['userId']
+              text: event.message['text']
             }
             client.reply_message(event['replyToken'], message)
           when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
