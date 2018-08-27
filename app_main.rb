4f9c8f60fcead248b28c67b6f8fe85a1292e46c3
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-27 17:41:34 +0900

    modify11

--- app_main.rb
+++ app_main.rb
@@ -28,10 +28,9 @@ post '/callback' do
           when Line::Bot::Event::MessageType::Text
             message = {
               type: 'text',
-              text: event.message['text']
+              text: event.source['userId'].to_s
             }
             client.reply_message(event['replyToken'], message)
-            File.write('event.json', event)
           when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
             response = client.get_message_content(event.message['id'])
             tf = Tempfile.open("content")
