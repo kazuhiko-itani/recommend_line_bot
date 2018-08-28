3c6492c5c37987de6c3559810d39eabb69573f4a
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-28 13:08:09 +0900

    create return_title_url_image

--- app_main.rb
+++ app_main.rb
@@ -56,13 +56,8 @@ get '/send' do
   protect!
 
   ranking_list = scraping_amazon_ranking
-  random_number = rand(0..19)
-
+  book_title, book_url, book_image = return_title_url_image(ranking_list)
   anounce_message = '【今日のおすすめの1冊】'
-  book_title = ranking_list[random_number][0].gsub(" ", "")
-  book_url = URI.decode(ranking_list[random_number][1]) + '/?openExternalBrowser=1'
-  book_url.gsub!('、', URI.encode('、'))
-  book_image = ranking_list[random_number][2]
 
   message = {
     type: 'text',
