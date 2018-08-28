b25676a3c28a9e8a718b845ff16f8772861e23de
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-28 12:20:11 +0900

    modify basic auth2

--- basic_auth.rb
+++ basic_auth.rb
@@ -9,6 +9,6 @@ def authorized?
   auth ||= Rack::Auth::Basic::Request.new(request.env)
   username = ENV['BASIC_AUTH_USERNAME']
   password = ENV["LINE_CHANNEL_TOKEN"]
-  auth.provided? && auth.basic?
-            && auth.credentials && auth.credentials == [username, password]
+  auth.provided? && auth.basic? && 
+            auth.credentials && auth.credentials == [username, password]
 end
