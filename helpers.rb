a54e0d04cde841b0fc98e0b958b62f067ee40f7d
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-28 13:15:51 +0900

    modify

--- helpers.rb
+++ helpers.rb
@@ -1,6 +1,7 @@
 def return_title_url_image(ranking_list)
   random_number = rand(0..19)
   book_title = ranking_list[random_number][0]
+  book_title.gsub!(' ', '')
   book_url = URI.decode(ranking_list[random_number][1]) + '/?openExternalBrowser=1'
   book_url.gsub!('、', URI.encode('、'))
   book_image = ranking_list[random_number][2]
