683003f6cd2ca2413a29430f5d35edc225c39a24
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-08-31 16:39:22 +0900

    modify5

--- app_main.rb
+++ app_main.rb
@@ -66,7 +66,7 @@ get '/send' do
 
   google_client = Google_drive.new
   rows_count = google_client.return_rows
-  p rows_count
+  puts rows_count
   #for count in 1..rows_count do
   #  client.push_message(get_user_id(count), message)
    # client.push_message(get_user_id(count), image)
